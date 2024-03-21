8931247d187e2610a8fb8e92e7454304f6997a20
ddt666 <450730239@qq.com>
2024-03-21 17:59:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -801,10 +801,13 @@ function socks5AddressParser(address) {
 function revertFakeInfo(content, userID, hostName, isBase64) {
 	if (isBase64) content = atob(content);//Base64解码
 	content = content.replace(new RegExp(fakeUserID, 'g'), userID).replace(new RegExp(fakeHostName, 'g'), hostName);
-	let lines = content.split('\n').map((line, index) => {
- 		 line = line +'-'+ index+'\n'
- 		 return line; // 如果需要返回处理后的行，可以在这里进行处理
-});
+// 	let lines = content.split('\n').map((line, index) => {
+// 		if (line){
+			
+//  		 line = line +'-'+ index+'\n'
+//  		 return line; // 如果需要返回处理后的行，可以在这里进行处理
+// 		}
+// });
 	 // if (isBase64) lines = btoa(lines);//Base64编码
 
 	return lines;
